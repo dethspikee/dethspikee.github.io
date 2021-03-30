622ebcc5912d443b19776ce1f365b99ace592f09
Damian Tabor <zettie008@gmail.com>
2021-03-30 18:14:03 +0100

    Add graph displaying current skills. Reindent

--- script.js
+++ script.js
@@ -2,60 +2,112 @@
  * Display into text to the user
  */
 const playIntro = () => {
-	const app = document.getElementById("app");
-	let typewriter = new Typewriter(app, {cursor: ' '});
+    const app = document.getElementById("app");
+    let typewriter = new Typewriter(app, {cursor: ' '});
 
-	setTimeout(() => {
-		typewriter = new Typewriter(app, {loop: false, delay: 50});
-		typewriter
-			.typeString('...A12"£!A:$....adw!"£"!£...')
-			.pauseFor(1500)
-			.deleteAll()
-			.typeString('Restoring connection...')
-			.pauseFor(1000)
-			.deleteAll()
-			.typeString('Connection established.')
-			.deleteAll()
-			.typeString('hell st@"£ger... try ')
-			.typeString('<strong>help</strong>')
-			.typeString(' to see available comm....')
-			.pauseFor(5000)
-			.deleteAll()
-			.typeString('Connection lost...')
-			.start();
+    setTimeout(() => {
+        typewriter = new Typewriter(app, {loop: false, delay: 50});
+        typewriter
+            .typeString('...A12"£!A:$....adw!"£"!£...')
+            .pauseFor(1500)
+            .deleteAll()
+            .typeString('Restoring connection...')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('Connection established...')
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString('Unknown user detected...')
+            .deleteAll()
+            .typeString('Welcome to dev-command')
+            .start();
 
-	}, 1000);
+    }, 1000);
+
+    setTimeout(() => {
+        const terminalWrapper = document.querySelector('.terminal-wrapper');
+        terminalWrapper.style.display = 'block';
+    }, 400);
 };
 
 
 document.addEventListener('DOMContentLoaded', event => {
-	playIntro();
+    playIntro();
 });
 
 
 $(function() {
-   $('#terminal').terminal({
-      help: function() {
-        this.echo('\n'); 
-        this.echo('\tWelcome to the DEV terminal'); 
-        this.echo('\n'); 
-        this.echo('Options:'); 
-        this.echo(''); 
-        this.echo('ls\t\t\t\tList all projects:'); 
-        this.echo('whoami\t\t\tContact me'); 
-      },
-      sub: {
-         get: function(arg) {
-         }
-      },
-      ls: function() {
-		  this.echo('Listing all projects...\n');
-	  },
-      close: function(arg1, arg2) {
-      },
-   },
-   {
-	   prompt: 'root@<xxx.xxx.xxx.xxx> ',
-	   greetings: false,
-   });
+    let intervalID = 0;
+    $('#terminal').terminal({
+        help: function() {
+            this.echo('\n'); 
+            this.echo('\tWelcome to the DEV terminal'); 
+            this.echo('\n'); 
+            this.echo('Options:'); 
+            this.echo(''); 
+            this.echo('ls\t\t\t\tList all projects:'); 
+            this.echo('whoami\t\t\tContact me'); 
+        },
+        sub: {
+            get: function(arg) {
+            }
+        },
+        ls: function() {
+            this.echo('Listing all projects...\n');
+        },
+        whoami: function() {
+            intervalID = showMyInfo(this);
+        },
+        close: function(arg1, arg2) {
+        },
+    },
+        {
+            prompt: '<unknown>@<xxx.xxx.xxx.xxx> ',
+            greetings: false,
+            keydown: function(command, term) {
+                if (command.keyCode == 17 || command.keyCode == 67) {
+                    clearInterval(intervalID);
+                }
+            },
+        });
 });
+
+const showMyInfo = (term) => {
+    let id = setInterval(() => {
+        term.clear();
+        term.echo("Always learning, Some might change over time. Try again...\n");
+        const django_length = Math.floor((Math.random() * 20) + 1);
+        const lang_length = Math.floor((Math.random() * 20) + 1);
+        const html_css_length = Math.floor((Math.random() * 10) + 1);
+        for (let i = 0; i < 10; i++) {
+            if (i == 2) {
+                term.echo("|-" + "-".repeat(django_length));
+                term.echo("| " + " ".repeat(django_length) + "|");
+                term.echo("| " + " ".repeat(django_length) + "|" + "DJANGO");
+                term.echo("| " + " ".repeat(django_length) + "|");
+                term.echo("|-" + "-".repeat(django_length));
+                continue;
+            }
+            if (i == 4) {
+                term.echo("|-" + "-".repeat(lang_length));
+                term.echo("| " + " ".repeat(lang_length) + "|");
+                term.echo("| " + " ".repeat(lang_length) + "|" + "PYTHON - JS - C");
+                term.echo("| " + " ".repeat(lang_length) + "|");
+                term.echo("|-" + "-".repeat(lang_length));
+                continue;
+            }
+
+            if (i == 6) {
+                term.echo("|-" + "-".repeat(html_css_length));
+                term.echo("| " + " ".repeat(html_css_length) + "|");
+                term.echo("| " + " ".repeat(html_css_length) + "|" + "HTLM - CSS");
+                term.echo("| " + " ".repeat(html_css_length) + "|");
+                term.echo("|-" + "-".repeat(html_css_length));
+                continue;
+            }
+            term.echo("|");
+        }
+        term.echo("0" + "-".repeat(30) + ">"); 
+    }, 3000);
+    return id;
+};
